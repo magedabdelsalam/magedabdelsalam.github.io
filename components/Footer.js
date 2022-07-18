cb3c274a4e4711603c2f5ce856998c1062564734
magedabdelsalam <dagadego@gmail.com>
2022-07-18 11:03:37 +0200

    Add cal.com link

--- components/Footer.js
+++ components/Footer.js
@@ -8,9 +8,10 @@ function Footer() {
             <p>A designer and developer based in Seattle.</p>
             <ul className={styles.socials}>
                 <li><a href="mailto:hello@magedabdelsalam.com">Email</a></li>
+                <li><a href="https://cal.com/maged">Chat</a></li>
                 <li><a href="https://github.com/magedabdelsalam">Github</a></li>
                 <li><a href="https://linkedin.com/in/magedabdelsalam">LinkedIn</a></li>
-                <li><a href="https://adplist.org/mentors/maged-abdelsalam">ADPList</a></li>
+                <li><a href="https://adplist.org/mentors/maged-abdelsalam">Mentorship</a></li>
             </ul>
             <p>© All rights reserved.</p>
         </footer>
