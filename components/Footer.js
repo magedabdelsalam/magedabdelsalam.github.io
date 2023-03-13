bd70193b3140cb6e7e91007ffbb5d0b22ba86088
magedabdelsalam <dagadego@gmail.com>
2023-03-13 12:57:10 +0200

    Updated Nextjs

--- components/Footer.js
+++ components/Footer.js
@@ -8,13 +8,13 @@ function Footer() {
         <footer>
             <hr/>
             <h2>About</h2>
-            <p>Maged Abdelsalam is a Product Designer based in Seattle.<br/>He&apos;s working on Konan AI at <Link href='https://www.synapse-analytics.io'><a>Synapse Analytics</a></Link>.</p>
+            <p>Maged Abdelsalam is a Product Designer based in Seattle.<br/>He&apos;s working on Konan AI at <Link href='https://www.synapse-analytics.io'>Synapse Analytics</Link>.</p>
             <ul className={styles.socials}>
-                <li><a href="https://cal.com/maged">Schedule a Call</a></li>
-                <li><a href="mailto:hello@magedabdelsalam.com">Email</a></li>
-                <li><a href="https://github.com/magedabdelsalam">Github</a></li>
-                <li><a href="https://linkedin.com/in/magedabdelsalam">LinkedIn</a></li>
-                <li><a href="https://adplist.org/mentors/maged-abdelsalam">ADPList</a></li>
+                <li><Link href="https://cal.com/maged">Schedule a Call</Link></li>
+                <li><Link href="mailto:hello@magedabdelsalam.com">Email</Link></li>
+                <li><Link href="https://github.com/magedabdelsalam">Github</Link></li>
+                <li><Link href="https://linkedin.com/in/magedabdelsalam">LinkedIn</Link></li>
+                <li><Link href="https://adplist.org/mentors/maged-abdelsalam">ADPList</Link></li>
             </ul>
             <hr/>
         </footer>
