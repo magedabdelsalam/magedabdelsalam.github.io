2f0b0c2fd1a27fba52f1b7ed3d42c2d9a3645c9e
Maged Abdelsalam <maged.abdelsalam@heircut.com>
2024-10-03 22:15:12 -0700

    Fixed package scripts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['localhost'], // Add any external domains you're loading images from
-    },
   }
   module.exports = nextConfig
